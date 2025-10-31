1f082b1c2d8e7d6a53365ac579856aa4e6239ae0
Codacy Archive <codacy-archive@local>
2025-10-31 01:59:12 +0000

    snapshot 2025-10-31T01:59:12.186348+00:00

---
+++ autogen.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+PROJECT=libiptcdata
+TEST_TYPE=-f
+FILE=libiptcdata/iptc-data.h
+
+DIE=0
+
+have_libtool=false
+if libtoolize --version < /dev/null > /dev/null 2>&1 ; then
+	have_libtool=true
+fi
+if $have_libtool ; then : ; else
+	echo
+	echo "You must have libtool installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/libtool/"
+	DIE=1
+fi
+
+(gtkdocize --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have gtk-doc installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at ftp://ftp.gnome.org/pub/GNOME/sources/gtk-doc/"
+	DIE=1
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/autoconf/"
+	DIE=1
+}
+
+if automake --version < /dev/null > /dev/null 2>&1 ; then
+    AUTOMAKE=automake
+    ACLOCAL=aclocal
+else
+	echo
+	echo "You must have automake installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+	DIE=1
+fi
+
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test $TEST_TYPE $FILE || {
+	echo "You must run this script in the top-level $PROJECT directory"
+	exit 1
+}
+
+if test -z "$AUTOGEN_SUBDIR_MODE"; then
+        if test -z "$*"; then
+                echo "I am going to run ./configure with no arguments - if you wish "
+                echo "to pass any to it, please specify them on the $0 command line."
+        fi
+fi
+
+ACLOCAL_FLAGS="-I m4"
+
+rm -rf autom4te.cache
+
+
+gtkdocize || exit $?
+
+autopoint || exit $?
+$ACLOCAL $ACLOCAL_FLAGS || exit $?
+libtoolize --force || exit $?
+$ACLOCAL $ACLOCAL_FLAGS || exit $?
+autoconf || exit $?
+autoheader || exit $?
+$AUTOMAKE --add-missing || exit $?
+gtkdocize || exit $?
+
+cd $ORIGDIR || exit $?
+
+if test -z "$AUTOGEN_SUBDIR_MODE"; then
+        $srcdir/configure --enable-maintainer-mode $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
+
+        echo 
+        echo "Now type 'make' to compile $PROJECT."
+fi
